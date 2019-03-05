af6001525c882c00096a6c409f030bbd00421479
Cadet <cadet@cadet-vm.ninja>
2019-03-05 11:34:53 -0500

    works tuesday

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,11 +5,11 @@ from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
-	def __str__(self):
-		return self.question_text
-        
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text  
+
     def was_published_recently(self): 
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
